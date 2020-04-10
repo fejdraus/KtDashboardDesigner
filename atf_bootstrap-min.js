af4ec2cbd21ba127081ef8c84cd52ae48cbfe300
Tyra Alexandr <AlTyra@beeline.kz>
2020-04-10 10:21:38 +0600

    Update atf_bootstrap-min.js

--- atf_bootstrap-min.js
+++ atf_bootstrap-min.js
@@ -4,4 +4,4 @@
  */
 
 
-requirejs.config({paths:{atf_dashboarddesigner:"https://raw.githubusercontent.com/fejdraus/KtDashboardDesigner/master/atf_dashboarddesigner-min",atf_dashboarddesigner_overrides:"https://raw.githubusercontent.com/fejdraus/KtDashboardDesigner/master/atf_dashboarddesigner_overrides-min.js"},shim:{atf_dashboarddesigner:{deps:["DashboardManager","DashboardDesignerViewGenerator","CommonCSSV2","PreviewableGridLayoutEdit","PreviewableGridLayoutEditItem","DashboardDesigner"]},atf_dashboarddesigner_overrides:{deps:["DashboardBuilder"]}}});require(["atf_dashboarddesigner_overrides"]);
+requirejs.config({paths:{atf_dashboarddesigner:"https://raw.githubusercontent.com/fejdraus/KtDashboardDesigner/master/atf_dashboarddesigner-min",atf_dashboarddesigner_overrides:"https://raw.githubusercontent.com/fejdraus/KtDashboardDesigner/master/atf_dashboarddesigner_overrides-min"},shim:{atf_dashboarddesigner:{deps:["DashboardManager","DashboardDesignerViewGenerator","CommonCSSV2","PreviewableGridLayoutEdit","PreviewableGridLayoutEditItem","DashboardDesigner"]},atf_dashboarddesigner_overrides:{deps:["DashboardBuilder"]}}});require(["atf_dashboarddesigner_overrides"]);
